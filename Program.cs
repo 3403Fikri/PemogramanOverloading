3ad900a85907357d5262e29b169e8fadca92f2ec
Muhammad Fikri Ramadhan <fikri@students.amikom.ac.id>
2021-04-01 06:23:38 +0700

    Memperbaiki bug

--- Program.cs
+++ Program.cs
@@ -7,10 +7,10 @@ namespace Overloading
         static void Main(string[] args)
         {
             Numbers number = new Numbers();
-            Console.WriteLine("Minimum #1: {0}",number.findMinimum(3,1));
-            Console.WriteLine("Minimum #2: {0}",number.findMinimum(3,2,4));
+            Console.WriteLine("Minimum #1: {0}", number.findMinimum(3,1));
+            Console.WriteLine("Minimum #2: {0}", number.findMinimum(3,2,4));
             Console.WriteLine();
-            Console.WriteLine("Maximum #1: {0}",number.findMaximum(3,1));
+            Console.WriteLine("Maximum #1: {0}", number.findMaximum(3,1));
             Console.WriteLine("Maximum #2: {0}", number.findMaximum(3,2,4));
         }
     }
